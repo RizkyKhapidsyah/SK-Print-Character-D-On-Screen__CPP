22fee4e35de18f033bbbec040f6d526748b68fa8
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-04-12 21:44:09 +0700

    Add project files.

---
+++ SK-Print-Character-D-On-Screen__CPP/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	cout << endl;
+	cout << "****" << endl;
+	cout << "*   *" << endl;
+	cout << "*   *" << endl;
+	cout << "****" << endl;
+
+	cout << endl;
+
+	_getch();
+	return 0;
+}
